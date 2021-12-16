2bb835404b7c9d5583f1913ee9b01ae3ce9b4269
kyle-mcr <kylemcreynolds92@gmail.com>
2021-12-16 08:53:09 -0800

    state manaement

--- src/Components/photowall.js
+++ src/Components/photowall.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import Photo from './photo';
+import propTypes from 'prop-types';
 
 function PhotoWall(props) {
     return <div>
-    {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+    {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
     </div>
+}
+
+PhotoWall.propTypes = {
+    posts: propTypes.array.isRequired,
+    onRemovePhoto: propTypes.func.isRequired
 }
 
 
